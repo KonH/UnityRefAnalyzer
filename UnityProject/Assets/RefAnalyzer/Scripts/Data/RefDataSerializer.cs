3e84b58e5c21755522691d9e2c8412e7adb195ee
konh <konh@yandex.ru>
2018-10-24 18:39:16 +0700

    Add pretty print to serializer;

--- UnityProject/Assets/RefAnalyzer/Scripts/Data/RefDataSerializer.cs
+++ UnityProject/Assets/RefAnalyzer/Scripts/Data/RefDataSerializer.cs
@@ -4,14 +4,16 @@ using UnityEngine.Assertions;
 namespace RefAnalyzer.Data {
 	public class RefDataSerializer {
 		readonly RefData _data;
-
-		public RefDataSerializer(RefData data) {
+		readonly bool    _prettyPrint;
+		
+		public RefDataSerializer(RefData data, bool prettyPrint = true) {
 			Assert.IsNotNull(data);
-			_data = data;
+			_data        = data;
+			_prettyPrint = prettyPrint;
 		}
 
 		public string Serialize() {
-			return JsonUtility.ToJson(_data);
+			return JsonUtility.ToJson(_data, _prettyPrint);
 		}
 	}
 }
