876a854c2be75c2fa68c06288f879ec2fd110bd4
konh <konh@yandex.ru>
2018-10-24 19:16:07 +0700

    Change text details;

--- RefAnalyzer/RefAnalyzer/RefAnalyzer.cs
+++ RefAnalyzer/RefAnalyzer/RefAnalyzer.cs
@@ -12,7 +12,7 @@ namespace RefAnalyzer {
 		private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
 		private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
 		private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-		private const string Category = "Naming";
+		private const string Category = "Unity";
 
 		private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
