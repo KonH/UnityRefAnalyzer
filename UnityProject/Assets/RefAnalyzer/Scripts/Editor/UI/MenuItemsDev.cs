1ca59a7200025ef3d3980cf921e8f185c7536ce6
KonH <konh@yandex.ru>
2018-11-06 19:40:50 +0700

    Add script to export .unitypackage with filtration;

---
+++ UnityProject/Assets/RefAnalyzer/Scripts/Editor/UI/MenuItemsDev.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace RefAnalyzer.UI {
+	public static class MenuItemsDev {
+		const string _fileName = "../package.unitypackage";
+
+		[MenuItem("RefAnalyzer/Export Package")]
+		static void ExportPackage() {
+			var allAssets = AssetDatabase.FindAssets("", new []{"Assets/RefAnalyzer"}).Select(guid => AssetDatabase.GUIDToAssetPath(guid)).ToArray();
+			var assetsToExport = allAssets.Where(path => !IsIgnoredPath(path)).ToArray();
+			Debug.Log("Exporting assets: \n" + string.Join(",\n", assetsToExport));
+			AssetDatabase.ExportPackage(assetsToExport, _fileName);
+			Debug.LogFormat("Assets exported to '{0}'", _fileName);
+		}
+
+		static bool IsIgnoredPath(string path) {
+			return path.ContainsAny("Scenes", "Examples", "Tests") || path.EndsWith("Dev.cs");
+		}
+
+		public static bool ContainsAny(this string str, params string[] parts) {
+			return parts.Any(p => str.Contains(p));
+		}
+	}
+}
